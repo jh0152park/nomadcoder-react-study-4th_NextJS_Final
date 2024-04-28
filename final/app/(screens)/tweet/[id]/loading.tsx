e9ff1530cc66b7be8820ec71b6bfb271e99dc5db
jh0152park <jh0152park@naver.com>
2024-04-28 19:07:27 +0900

    added post detail loading page

--- final/app/(screens)/tweet/[id]/loading.tsx
+++ final/app/(screens)/tweet/[id]/loading.tsx
@@ -1,3 +1,7 @@
 export default function Loading() {
-    return null;
+    return (
+        <div className="flex flex-col gap-2 p-5 animate-pulse w-full max-w-[430px] ">
+            <div className="w-full mt-14 rounded-md h-96 bg-neutral-400 " />
+        </div>
+    );
 }
